da18929762ef3c1805c40fd98f39b4703e38a106
Tom <tomjrodrigues@gmail.com>
2017-01-18 14:21:44 -0800

    done with lessons 00-02

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,10 @@
-#write your code here
+def ftoc(f)
+	output = (f - 32.0) * 5.0 / 9.0
+	return output
+end
+
+def ctof(c)
+	output = c * 9.0 / 5.0 + 32.0
+	return output
+end
+
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,45 @@
-#write your code here
+def add(number1, number2)
+	number1 + number2
+end
+
+def subtract(number1, number2)
+	number1 - number2
+end
+
+def sum(numbers)
+	sum = 0
+	numbers.each do |n| 
+		sum += n
+	end
+	sum
+end
+
+def multiply(numbers)
+	output = 1
+	numbers.each do |n|
+		output *= n
+	end
+	output
+end
+
+def power(number1, number2)
+	output = 1
+	if number2 == 0
+		return 1
+	else
+		for i in 1..number2
+			output *= number1
+		end
+	output
+	end
+end
+
+def factorial(number)
+	output = 1
+	for i in 1..number
+		output *= i
+	end
+	output
+end
+
+
